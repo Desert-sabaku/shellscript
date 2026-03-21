75f8a2c8a785f8042d53cd2320ad7907fc958c2b
SANADA Euki <yuu.mat.930@gmail.com>
2026-03-21 23:37:29 +0900

    feat: Show an anwer with the index.

--- fizzbuzz.sh
+++ fizzbuzz.sh
@@ -2,11 +2,11 @@
 
 for i in $(seq 1 100); do
 	if [ $((i % 15)) -eq 0  ]; then
-		echo FizzBuzz
+		echo $i FizzBuzz
 	elif [ $((i % 3)) -eq 0 ]; then
-		echo Fizz
+		echo $i Fizz
 	elif [ $((i % 5)) -eq 0 ]; then
-		echo Buzz
+		echo $i Buzz
 	else
 		echo $i
 	fi
